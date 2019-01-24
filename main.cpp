ca6a201a78dbab68d378c77047929bdc54e6cab9
PeterHAuCl4 <39565717+PeterHAuCl4@users.noreply.github.com>
2019-01-23 18:17:59 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,58 +8,49 @@
 #include <iostream>
 #include <cmath> //Para utilizar abs() con cualquier número
 #include <iomanip> //Agregar la presición deseada
-#include <time.h> //Para medir el tiempo de ejecución del programa
+#include <time.h>
 #define e 0.0001 //Se maneja el error como una constante que no puede cambiar
-
-
 using namespace std;
 
-static double calcular(double p_i_aux, double a, double b); //Función recursiva para calcular P_n
-static double f(double equis); //Función para ejecutar la función  f(x) = x³ + 4x² - 10
-int sumar(int* i); //Función para agregar números al contador 'i'
-static int i = 1; //Se declara la variable de forma global para que se pueda llamar desde cualquier función
+double f(double equis); //Función para ejecutar la función  f(x) = x³ + 4x² - 10
 
 int main(){
-
-    clock_t start = clock();
-    double a = 1, b = 2 , p_i_aux = 0;
-
-    calcular(p_i_aux, a, b);
-
-    cout << "Tiempo transcurrido: " << clock()-start << endl;
-    }
-
-double calcular(double p_i_aux, double a, double b){
+clock_t start = clock();
+    double a = 1, b = 2 , p_i = 0 , p_i_aux = 0 , absol = 0;
+    bool ceja = false;
+    int i = 1;
 
     /* La variable p_i es P_n y p_i_aux es P_(n-1), absol es la variable que contiene el error
      *
+     *
      * */
-    double p_i = (a + b)/2;
 
-    double absol = abs(p_i - p_i_aux)/abs(p_i); //Aquí se calcula el error relativo
+    while(ceja == false){
 
-    cout << setprecision(7) << "i: " << i << " a: " << a << " b: " << b << " Pn: " << p_i
-         << " f(an): " << f(a) << " f(Pn): " << f(p_i)<< " Err: " << absol << endl;
+        p_i = (a + b)/2;
 
-    if(absol < e){
-        return p_i; //Es para salir de la función
-    }else{
+        absol = abs(p_i - p_i_aux)/abs(p_i); //Aquí se calcula el error relativo
+
+                cout << setprecision(7) << "i: " << i << " a: " << a << " b: " << b << " Pn: " << p_i
+                     << " f(an): " << f(a) << " f(Pn): " << f(p_i)<< " Err: " << absol << endl;
 
         if(f(p_i)*f(a) > 0){ //Si los valores de la función evaluada en P_n y 'a' son positivos, 'a' se convierte en P_n
             a = p_i;
         }else{ //De lo contrario 'b' se convierte en P_n
          b = p_i;
         }
-        p_i_aux = p_i; // Se asigna el valor de P_n a P_(n-1)
-        i+=1;
-        return calcular(p_i_aux, a, b); //En caso de que no se cumpla la condición, se genera la recursividad.
-    }
-}
 
+        if(absol < e /*|| abs(f(p_i)) < e*/){  /* A pesar de que en el libro se pide que se use de esta forma, para que salgan
+            exactamente 13 iteraciones se debe quitar la segunda condición */
+
+            ceja = true; // Para salir del ciclo
+        }
+        p_i_aux = p_i; // Se asigna el valor de P_n a P_(n-1)
+        i++;
+    }
+
+    cout << "Tiempo transcurrido: " << clock()-start << endl;
+}
 double f(double equis){
     return pow(equis , 3) + 4*pow(equis, 2) - 10;
-}
-int sumar(int* i){
-    *i+=1;
-    return *i;
 }
